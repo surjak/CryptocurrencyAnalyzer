48494ea2081d47090ca55bbecd5988becfad0fd0
surjak <surjak1999@gmail.com>
2020-05-17 17:03:35 +0200

    removed JWT

--- app/controllers/AppController.scala
+++ app/controllers/AppController.scala
@@ -1,6 +1,5 @@
 package controllers
 
-import config.JwtUtility
 import javax.inject.{Inject, Singleton}
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import play.api.i18n.I18nSupport
@@ -14,15 +13,8 @@ class AppController @Inject()(cc: ControllerComponents, protected val dbConfigPr
   extends AbstractController(cc) with HasDatabaseConfigProvider[JdbcProfile] with I18nSupport {
 
   def app = Action { implicit req =>
-    val jwtToken = req.session.get("jwtAuth").getOrElse("")
-    if (JwtUtility.isValidToken(jwtToken)) {
-      JwtUtility.decodePayload(jwtToken).fold {
-        println("Fail")
-      } {
-        payload =>
-          println(payload)
-      }
-    }
+    val email = req.session.get("email").getOrElse("zle")
+    println(email)
     Ok(views.html.app())
   }
 

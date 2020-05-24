bc6bf2b6278f6e02c5dd0e7607a7b74c84525ca0
surjak <surjak1999@gmail.com>
2020-05-24 22:03:16 +0200

    adding constraints

--- app/controllers/AppController.scala
+++ app/controllers/AppController.scala
@@ -2,15 +2,27 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 import models.Tables._
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.data.format.Formats._
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import play.api.i18n.I18nSupport
 import play.api.libs.json.Json
 import play.api.mvc.{AbstractController, ControllerComponents}
-import services.AirlyDriver
 import slick.jdbc.JdbcProfile
 import slick.jdbc.PostgresProfile.api._
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
+
+case class ConstraintsForm(pollutionType: String, value: Double)
+
+object ConstraintsForm {
+  val form: Form[ConstraintsForm] = Form(mapping(
+    "Pollution Type" -> text,
+    "Value" -> of(doubleFormat)
+  )(ConstraintsForm.apply)(ConstraintsForm.unapply))
+}
+
 
 @Singleton
 class AppController @Inject()(cc: ControllerComponents, protected val dbConfigProvider: DatabaseConfigProvider)(implicit ex: ExecutionContext)
@@ -20,9 +32,9 @@ class AppController @Inject()(cc: ControllerComponents, protected val dbConfigPr
     val email = req.session.get("email").getOrElse("zle")
     //    println(email)
     //    EmailSender.sendEmail("radek4ec@gmail.com", "Tutaj EmailAlert\nCos sie stalo");
-    val obj = AirlyDriver.getParameterValue()
+    //    val obj = AirlyDriver.getParameterValue()
 
-    Ok(views.html.configuration())
+    Ok(views.html.configuration(ConstraintsForm.form))
   }
 
   def loadUserLonAndLat = Action { implicit req =>
@@ -32,6 +44,21 @@ class AppController @Inject()(cc: ControllerComponents, protected val dbConfigPr
     val lon = (json.get \ "lon").get.toString().toDouble
     db.run(Users.filter(userRow => userRow.email === email).map(u => (u.lat, u.lon)).update(Option(lat), Option(lon)))
     Ok(Json.toJson("{status: 'ok'}"))
+  }
+
+  def createConstraint = Action.async { implicit req =>
+    ConstraintsForm.form.bindFromRequest.fold(
+      formWithErrors => Future.successful(BadRequest(views.html.configuration(formWithErrors)))
+      ,
+      formdata => {
+        val email = req.session.get("email")
+        val userId = db.run(Users.filter(userRow => userRow.email === email).map(u => u.id).result)
+        userId.map(id => id.head).map(id =>
+          db.run(Constraints += ConstraintsRow(id, formdata.pollutionType, Option(formdata.value))))
+
+        Future.successful(Redirect(routes.AppController.createConstraint()))
+      }
+    )
   }
 
 }
